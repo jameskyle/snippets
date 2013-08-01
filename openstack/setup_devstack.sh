33a7db32c6f5b53027053c32e63fe5212ee04a27
James Kyle <james@jameskyle.org>
2013-08-01 15:23:41 -0700

    Remove old oslo first.

--- openstack/setup_devstack.sh
+++ openstack/setup_devstack.sh
@@ -7,4 +7,6 @@ curl -O https://raw.github.com/jameskyle/snippets/master/openstack/local.sh
 curl -O https://raw.github.com/jameskyle/snippets/master/openstack/localrc
 
 # FIXFOR::  https://bugs.launchpad.net/neutron/+bug/1206013
+cd /usr/local/lib/python2.7/dist-packages
+sudo rm -rf oslo*
 sudo pip install --upgrade http://tarballs.openstack.org/oslo.config/oslo.config-1.2.0a3.tar.gz#egg=oslo.config-1.2.0a3
